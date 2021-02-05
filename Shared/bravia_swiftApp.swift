304d2ca9f0aaac3171ea3e5cd8b7b3b249a41802
iTh1nk <fnchao@hotmail.com>
2021-02-05 14:51:12 -0800

    Initial Commit

---
+++ Shared/bravia_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  bravia_swiftApp.swift
+//  Shared
+//
+//  Created by Chao Feng on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct bravia_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
